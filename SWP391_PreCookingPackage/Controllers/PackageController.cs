be1af108d05fb53b5f48c6cd5a11a087809a697f
noday-off <thinn2002@gmail.com>
2023-05-19 13:23:53 +0700

    test new controller

---
+++ SWP391_PreCookingPackage/Controllers/PackageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWP391_PreCookingPackage.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PackageController : ControllerBase
+    {
+        [HttpGet]
+        public String GetPackages() {
+            return "Test package";
+        }
+    }
+}
